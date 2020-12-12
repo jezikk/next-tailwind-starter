1afa343440d3c06377982c30b498ff0b33721fc0
Michal Vrbata <vrbata@outlook.com>
2020-12-12 18:14:19 +0100

    Init setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwindcss-question-mark'),
+    //require('tailwindcss-aspect-ratio'),
+  ],
+};
